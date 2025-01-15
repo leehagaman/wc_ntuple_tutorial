d7a52581b2c213965884f02da37d0d394a97856e
leehagaman <62082849+leehagaman@users.noreply.github.com>
2025-01-15 12:58:00 -0600

    added multiple runs

--- README.md
+++ README.md
@@ -9,11 +9,10 @@ Created for the September 2024 MicroBooNE Analysis Workshop at the University of
 git clone https://github.com/leehagaman/wc_ntuple_tutorial
 cd wc_ntuple_tutorial
 pip install requirements.txt # or install any imports that error with conda/pip
-scp YOUR_FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_data_bnb_run1_PF.root data
-scp YOUR_FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_data_extbnb_run1_PF.root data
-scp YOUR_FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_prodgenie_bnb_dirt_overlay_run1_PF.root data
-scp YOUR_FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_prodgenie_bnb_nu_overlay_run1_PF.root data
+source run_1_download_script.sh YOUR_FNAL_USERNAME # needs a valid token from kinit first
 ```
 
-# Look at tutorial.ipynb to see examples and explanations about these root files!
+`tutorial.ipynb` walks through some examples and explanations about these root files.
+
+`tutorial_multiple_runs.ipynb` shows examples with runs 1-3, including correctly normalizing by POT. To use these, run `source runs_123_download_script.sh YOUR_FNAL_USERNAME`
 
---
+++ run_1_download_script.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+FNAL_USERNAME=$1
+
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_data_bnb_run1_PF.root data
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_data_extbnb_run1_PF.root data
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_prodgenie_bnb_dirt_overlay_run1_PF.root data
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_prodgenie_bnb_nu_overlay_run1_PF.root data
+
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_data_bnb_run2_PF.root data
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_data_extbnb_run2_PF.root data
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_prodgenie_bnb_dirt_overlay_run2_PF.root data
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_prodgenie_bnb_nu_overlay_run2_PF.root data
+
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_data_bnb_run3_PF.root data
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_data_extbnb_run3_PF.root data
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_prodgenie_bnb_dirt_overlay_run3_PF.root data
+scp $FNAL_USERNAME@uboonegpvm01.fnal.gov:/exp/uboone/data/users/bbogart/processed_checkout_rootfiles/checkout_prodgenie_bnb_nu_overlay_run3_PF.root data
